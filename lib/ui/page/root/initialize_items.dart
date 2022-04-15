a24f0556c0d0b08949938c8b11ce3e8f79b98fa2
jiangyu <578154186@qq.com>
2022-04-15 16:36:44 +0800

    姜宇 tabbar

--- lib/ui/page/root/initialize_items.dart
+++ lib/ui/page/root/initialize_items.dart
@@ -4,6 +4,7 @@ import 'package:flutter_pajkb/ui/page/health/health.dart';
 import 'package:flutter_pajkb/ui/page/home/home.dart';
 import 'package:flutter_pajkb/ui/page/mine/mine.dart';
 import 'package:flutter_pajkb/ui/page/product/product.dart';
+import 'package:flutter_pajkb/ui/shared/size_fit/int_extension.dart';
 
 final List<Widget> pages = [
   JKBHomePage(),
@@ -16,22 +17,27 @@ final List<Widget> pages = [
 final List<BottomNavigationBarItem> items = [
   BottomNavigationBarItem(
       label: "首页",
-      icon: Icon(Icons.home)
+      icon: Image.asset("assets/images/tabbar/tab_button_shouye_normal@2x.png", width: 20.px,),
+      activeIcon: Image.asset("assets/images/tabbar/tab_button_shouye_press@2x.png", width: 20.px,)
   ),
   BottomNavigationBarItem(
       label: "保险",
-      icon: Icon(Icons.star)
+      icon: Image.asset("assets/images/tabbar/tab_button_baoxian_normal@2x.png", width: 20.px,),
+      activeIcon: Image.asset("assets/images/tabbar/tab_button_baoxian_press@2x.png", width: 20.px,)
   ),
   BottomNavigationBarItem(
       label: "健康",
-      icon: Icon(Icons.star)
+      icon: Image.asset("assets/images/tabbar/tab_button_jiankang_normal@2x.png", width: 20.px,),
+      activeIcon: Image.asset("assets/images/tabbar/tab_button_jiankang_press@2x.png", width: 20.px,)
   ),
   BottomNavigationBarItem(
       label: "专家",
-      icon: Icon(Icons.star)
+      icon: Image.asset("assets/images/tabbar/tab_button_jiuyi_normal@2x.png", width: 20.px,),
+      activeIcon: Image.asset("assets/images/tabbar/tab_button_jiuyi_press@2x.png", width: 20.px,)
   ),
   BottomNavigationBarItem(
       label: "我的",
-      icon: Icon(Icons.star)
+      icon: Image.asset("assets/images/tabbar/tab_button_wode_normal@2x.png", width: 20.px,),
+      activeIcon: Image.asset("assets/images/tabbar/tab_button_wode_press@2x.png", width: 20.px,)
   )
 ];
--- lib/ui/page/root/root.dart
+++ lib/ui/page/root/root.dart
@@ -23,6 +23,8 @@ class _JKBRootPageState extends State<JKBRootPage> {
       bottomNavigationBar: BottomNavigationBar(
         unselectedItemColor: Colors.grey,//主要用于设置item及字体颜色
         selectedItemColor: Colors.orange,//主要用于设置item及字体颜色
+        selectedFontSize: 14.0,
+        unselectedFontSize: 14.0,
         currentIndex: _currentIndex,
         type: BottomNavigationBarType.fixed,
         items: items,
